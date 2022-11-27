2304b5a54cc01eb04031d52e8a0804ca631a0499
Antonin SIMON <antonin.simon@epsi.fr>
2022-11-27 16:16:12 +0100

    update

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -13,9 +13,9 @@ import CitationsCP from '../assets/AirZen/commpositive/Citations.png'
 
 //Swapper CAVE
 import AllProductsCAVE from '../../src/assets/CAVE/CAVE_allproducts.PNG'
-import MapCAVE from '../assets/CAVE/CAVE_map.PNG'
-import AddProductCAVE from '../assets/CAVE/CAVE_addproduct.PNG'
-import ProductCAVE from '../assets/CAVE/CAVE_product.PNG'
+import MapCAVE from '../../src/assets/CAVE/CAVE_map.PNG'
+import AddProductCAVE from '../../src/assets/CAVE/CAVE_addproduct.PNG'
+import ProductCAVE from '../../src/assets/CAVE/CAVE_product.PNG'
 
 // TODO remove mousefollow when it's not necessarry
 
